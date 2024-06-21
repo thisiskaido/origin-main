d7bcfe2f144cfdcaff1376c5a05f240fda5bb3b1
Danill Karpachev <karpachev.dv@icloud.com>
2024-06-21 11:06:36 +0300

    add some variables l1:task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Hello, World!");
+
+using System.Reflection.Metadata;
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.5;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
